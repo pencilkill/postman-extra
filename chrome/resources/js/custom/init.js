843b1cebd786e0bda6d71bc8d573054013ef6f59
Sam <mail.song.de.qiang@gmail.com>
2017-05-22 17:11:24 +0800

    allow window.open (tests)

--- chrome/resources/js/custom/init.js
+++ chrome/resources/js/custom/init.js
@@ -1,3 +1,9 @@
+/** Allow window.open() **/
+Evaluator = Evaluator.extend({    
+    shimWindowFunctions: function() {}
+});
+
+/** **/
 var uc = new UC();
 var cs = new CS();
 var chat = new CHAT();
